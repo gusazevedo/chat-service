edd100bd3d612329843638970ce8fe81c218d9a9
Gustavo Azevedo <gustavoazevedogan@gmail.com>
2023-04-21 16:39:40 -0300

    Implementa entidade de models

--- internal/domain/entity/model.go
+++ internal/domain/entity/model.go
@@ -1,1 +1,26 @@
 package entity
+
+// Constutor
+type Model struct {
+	Name      string
+	Maxtokens int
+}
+
+// Classe
+
+func NewModel(name string, maxTokens int) *Model {
+	return &Model{
+		Name:      name,
+		Maxtokens: maxTokens,
+	}
+}
+
+// Métodos
+
+func (m *Model) GetMaxTokens() int {
+	return m.Maxtokens
+}
+
+func (m *Model) GetModelName() string {
+	return m.Name
+}
